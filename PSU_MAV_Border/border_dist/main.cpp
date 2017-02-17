beaa20c7fcd431c3731cbc9af220e814a24db99e
Kalki Sharma <kalkijsharma@gmail.com>
2017-02-16 19:46:57 -0500

    MAV Altitude
    Border distance requires slope and y-intercept of line and MAV altitude.

--- PSU_MAV_Border/border_dist/main.cpp
+++ PSU_MAV_Border/border_dist/main.cpp
@@ -12,59 +12,66 @@
 using std::endl;
 using std::cout;
 
-/* Declare functions */
-double get_length(double x1, double y1, double x2, double y2);
-double get_y(double x);
-double get_dist(double a, double b, double c);
+//
+class Distance{
+private:
+  double theta = 53;        // Camera half-angle in x-direction (640 pixels)
+  double phi = 40;          // Camera half-angle in y-direction (480 pixels)
+  double xMax = 640.0;      // Maximum x-axis grid location
+  double yMax = 480.0;      // Maximum y-axis grid location
+public:
+  //Distance() : {}
+  /* Compute shortest distance to border given lengths of the triangle */
+  double get_dist(double m, double b, double h){
+    double x1, x2, x3, y1, y2, y3, len_a, len_b, len_c, area, s, r_pix, r_m;
 
-/* Declare constants */
-const double xMax = 640.0;      // Maximum x-axis grid location
-const double yMax = 480.0;      // Maximum y-axis grid location
-const double quad_coord[2] = {xMax/2.0, yMax/2.0};    // Grid location of quad
-const double m = 10.0;          // Slope of border line
-const double b = 1.0;           // Y intercept of border line
+    x1 = 0.0;
+    x2 = xMax/2.0;
+    x3 = x2;
+    y2 = yMax/2.0;
+
+    y1 = get_y(x1, m, b);
+    y3 = get_y(x3, m, b);
+
+    len_a = get_length(x1, y1, x2, y2);
+    len_b = get_length(x1, y1, x3, y3);
+    len_c = get_length(x2, y2, x3, y3);
+
+    s = 0.5*(len_a + len_b + len_c);
+    area = sqrt(s*(s - len_a)*(s - len_b)*(s - len_c));
+    r_pix = (2*area/len_b);
+    r_m = r_pix * 2 * h / (sin(theta*3.14/180.0) * 640.0);
+    return r_m;
+  }
+  /* Compute y distance on border corresponding to an x location on the grid */
+  double get_y(double x, double m, double b){
+    double y;
+
+    y = m*x + b;
+
+    return (y);
+  }
+  /* Compute length of side of triangle given two grid points */
+  double get_length(double x1, double y1, double x2, double y2){
+
+    return sqrt(pow((x1-x2),2.0) + pow((y1-y2),2.0));
+  }
+};
 //------------------------------------------------------------------------------
 int main(){
-  double len_a, len_b, len_c, r;
+  double slope;           // Slope of border line
+  double y_intercept;      // Y intercept of border line
+  double alt;             // MAV altitude
+  double r;
 
-  /* Compute leg of triangle */
-  len_a = get_length(0.0, get_y(0.0), quad_coord[0], quad_coord[1]);
-  cout << "Length a: " << len_a << endl;
+  slope = 15.0;
+  y_intercept = 220.0;
+  alt = 1.0;
 
-  /* Compute base of triangle */
-  len_b = get_length(quad_coord[0], get_y(quad_coord[0]), 0.0, get_y(0.0));
-  cout << "Length b: " << len_b << endl;
-
-  /* Compute leg of triangle */
-  len_c = get_length(quad_coord[0], quad_coord[1], quad_coord[0], get_y(quad_coord[0]));
-  cout << "Length c: " << len_c << endl;
+  Distance dist;
 
   /* Compute shortest distance of quad from border */
-  r = get_dist(len_a, len_b, len_c);
-  cout << "Shortest distance to border: " << r << endl;
-}
-//------------------------------------------------------------------------------
-/* Compute y distance on border cooresponding to an x location on the grid */
-double get_y(double x){
-  double y;
-
-  y = m*x + b;
-
-  return (y);
-}
-//------------------------------------------------------------------------------
-/* Compute length of side of triangle given two grid points */
-double get_length(double x1, double y1, double x2, double y2){
-
-  return sqrt(pow((x1-x2),2.0) + pow((y1-y2),2.0));
-}
-//------------------------------------------------------------------------------
-/* Compute shortest distance to border given lengths of the triangle */
-double get_dist(double a, double b, double c){
-  double area, s;
-
-  s = 0.5*(a + b + c);
-  area = sqrt(s*(s - a)*(s - b)*(s - c));
-  return (2*area/b);
+  r = dist.get_dist(slope, y_intercept, alt);
+  cout << "Shortest distance to border: " << r << "m" << endl;
 }
 //------------------------------------------------------------------------------
